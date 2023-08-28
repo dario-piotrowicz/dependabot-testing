990cfa5a46a314c026b1c1900fa42206c7527a47
Dario Piotrowicz <dario@cloudflare.com>
2023-08-28 23:21:35 +0100

    implement idea

---
+++ .github/get-c3-dependabot-bumped-framework.cjs
@@ -0,0 +1,16 @@
+module.exports = function(prDescription){
+    const json = require('./packages/create-cloudflare/src/frameworks/package.json')
+
+    const frameworkCliPackages = Object.values(json.frameworkCliMap);
+
+    const semverRegexStr = '\\d+\\.\\d+\\.\\d+';
+
+    const frameworkCliRegex = new RegExp(
+      `(?:^|\\s+)Bumps\\s+\\[(${frameworkCliPackages.join(
+        '|'
+      )})\\]\\(.*?\\)\\s+from\\s+${semverRegexStr}\\s+to\\s+${semverRegexStr}`
+    );
+
+    const bumpedFrameworkCli = prDescription.match(frameworkCliRegex)?.[1];
+    return bumpedFrameworkCli;
+};
