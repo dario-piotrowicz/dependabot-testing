73b9d279ea219fdbfc4dc7d13a74c0c6fb3f24d5
Dario Piotrowicz <dario@cloudflare.com>
2024-01-02 22:22:34 +0100

    apply change to get bumped framework cjs

--- .github/get-c3-dependabot-bumped-framework.cjs
+++ .github/get-c3-dependabot-bumped-framework.cjs
@@ -1,16 +1,44 @@
-module.exports = function(prDescription){
-    const json = require('../packages/create-cloudflare/src/frameworks/package.json')
+const { execSync } = require('child_process');
 
-    const frameworkCliPackages = Object.values(json.frameworkCliMap);
+module.exports = function(){
+  const diff = execSync(
+    "git diff HEAD~1 packages/create-cloudflare/src/frameworks/package.json"
+  ).toString();
 
-    const semverRegexStr = '\\d+\\.\\d+\\.\\d+';
+  const changedPackages =
+    diff
+      .match(/-\s*".*?":\s".*?",?/g)
+      ?.map((match) => match.match(/-\s*"(.*)":/)?.[1])
+      .filter(Boolean) ?? [];
 
-    const frameworkCliRegex = new RegExp(
-      `(?:^|\\s+)Bumps\\s+\\[(${frameworkCliPackages.join(
-        '|'
-      )})\\]\\(.*?\\)\\s+from\\s+${semverRegexStr}\\s+to\\s+${semverRegexStr}`
-    );
+  const changes = changedPackages.map((pkg) => {
+    const getPackageChangeRegex = (addition) =>
+      new RegExp(`${addition ? "\\+" : "-"}\\s*"${pkg}":\\s"(.*)",?`);
 
-    const bumpedFrameworkCli = prDescription.match(frameworkCliRegex)?.[1];
-    return bumpedFrameworkCli;
+    const from = diff.match(getPackageChangeRegex(false))?.[1];
+    const to = diff.match(getPackageChangeRegex(true))?.[1];
+
+    if (!from || !to) {
+      throw new Error(
+        `Unexpected changes for package ${pkg} (could not determine upgrade)`
+      );
+    }
+
+    return {
+      package: pkg,
+      from,
+      to,
+    };
+  });
+
+  if (changes.length === 0) {
+    console.warn("No changes detected!");
+    return null;
+  } else if(changes.length > 1){
+    console.warn("More then one package has changes, that's not currently supported");
+    throw new Error('More than one change detected');
+  } else {
+    console.log(`%%%%%%+++++++>>>>> ${changes[0].package}`);
+    return changes[0].package;
+  }
 };
