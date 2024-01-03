bda82df18864d8130e3981d15cf62d49f280d3bd
Dario Piotrowicz <dario@cloudflare.com>
2024-01-03 12:47:23 +0100

    simplify get bumped framework

--- .github/get-c3-dependabot-bumped-framework.cjs
+++ .github/get-c3-dependabot-bumped-framework.cjs
@@ -1,48 +1,23 @@
 const { execSync } = require('child_process');
 
 module.exports = function(){
-  const numOfNewCommitInBranch = parseInt(execSync("git rev-list --count HEAD").toString()) - 1;
-
-  console.log(`numOfNewCommitInBranch=${numOfNewCommitInBranch}`);
-  
   const diff = execSync(
-    `git diff HEAD~${numOfNewCommitInBranch} packages/create-cloudflare/src/frameworks/package.json`
+    `git diff HEAD~1 packages/create-cloudflare/src/frameworks/package.json`
   ).toString();
 
   const changedPackages =
-	diff
-		.match(/-\s*".*?":\s".*?",?/g)
-		?.map((match) => match.match(/-\s*"(.*)":/)?.[1])
-		.filter(Boolean) ?? [];
-  
-  const changes = changedPackages.map((pkg) => {
-    const getPackageChangeRegex = (addition) =>
-      new RegExp(`${addition ? "\\+" : "-"}\\s*"${pkg}":\\s"(.*)",?`);
+    diff
+        .match(/-\s*".*?":\s".*?",?/g)
+        .map((match) => match.match(/-\s*"(.*)":/)?.[1])
+        .filter(Boolean);
 
-    const from = diff.match(getPackageChangeRegex(false))?.[1];
-    const to = diff.match(getPackageChangeRegex(true))?.[1];
-
-    if (!from || !to) {
-      throw new Error(
-        `Unexpected changes for package ${pkg} (could not determine upgrade)`
-      );
-    }
-
-    return {
-      package: pkg,
-      from,
-      to,
-    };
-  });
-
-  if (changes.length === 0) {
+  if (changedPackages.length === 0) {
     console.warn("No changes detected!");
     return null;
-  } else if(changes.length > 1){
-    console.warn("More then one package has changes, that's not currently supported");
+  } else if(changedPackages.length > 1){
+    console.warn("More then one package has changed, that's not currently supported");
     throw new Error('More than one change detected');
   } else {
-    console.log(`%%%%%%+++++++>>>>> ${changes[0].package}`);
-    return changes[0].package;
+    return changedPackages[0];
   }
 };
